74573ae0c3be44c9b85c7fadc8e7f2d179407dd1
Varun Krishna S <varunhawk19@gmail.com>
2022-05-21 16:05:40 +0530

    wip

---
+++ chat/views.py
@@ -0,0 +1,54 @@
+from django.shortcuts import render
+from rest_framework import viewsets
+from chat.models import Room, Message
+from django.contrib.auth.models import User
+from chat.serializers import MessageSerializer, RoomSerializer, UserSerializer
+from rest_framework.response import Response
+from rest_framework import status #/ Create your views here.
+class UserViewset(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class =UserSerializer
+
+    # @action(detail=True, methods=['post'])
+    # def set_password(self, request, pk=None):
+    #     user = self.get_object()
+    #     serializer = PasswordSerializer(data=request.data)
+    #     if serializer.is_valid():
+    #         user.set_password(serializer.validated_data['password'])
+    #         user.save()
+    #         return Response({'status': 'password set'})
+    #     else:
+    #         return Response(serializer.errors,
+    #                         status=status.HTTP_400_BAD_REQUEST)
+
+    # @action(detail=False)
+    # def recent_users(self, request):
+    #     recent_users = User.objects.all().order_by('-last_login')
+
+    #     page = self.paginate_queryset(recent_users)
+    #     if page is not None:
+    #         serializer = self.get_serializer(page, many=True)
+    #         return self.get_paginated_response(serializer.data)
+
+    #     serializer = self.get_serializer(recent_users, many=True)
+    #     return Response(serializer.data)
+class ChatRoomViewset(viewsets.ModelViewSet):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
+    
+    def create(self, request, *args, **kwargs):
+        print(request.data)
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        obj = self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(self.get_serializer(obj).data, status=status.HTTP_201_CREATED, headers=headers)
+
+    def perform_create(self, serializer):
+        print(serializer)
+        return serializer.save()
+
+
+class MessageViewset(viewsets.ModelViewSet):
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
